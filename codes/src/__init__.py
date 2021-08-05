d8aa0a3ce65c3c4cb8cd0b219b237a14d751eb44
Vedant <vedantjadhav5566@gmail.com>
2021-08-05 18:34:05 +0530

    Added login manager

--- codes/src/__init__.py
+++ codes/src/__init__.py
@@ -1,6 +1,25 @@
 from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+#########################################################
+""" Remove it later """
+import os
+
+DB_PATH = os.path.abspath(os.getcwd() + "/src/temp.db")
+#########################################################
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "NnuUKdVF71sppI6cia7XXf386_AbYUnnXnmdaOW_PRy_sGawZXfScC_DoGNAO0r6H4WgNwGGeCnaXxevUapfyw"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + DB_PATH
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+from src.models import User
+@login_manager.user_loader
+def load_user(username):
+    return User.query.get(username)
 
 from src import routes
