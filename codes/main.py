8f017c645ffa389521d12a4435f375cf83cceabe
Vedant <vedantjadhav5566@gmail.com>
2021-08-11 22:37:39 +0530

    Debug 4

--- codes/main.py
+++ codes/main.py
@@ -1,9 +1,4 @@
 from src import app
-from src.jobs import loop
-from threading import Thread
 
 if __name__ == '__main__':
-    Thread(target=loop, daemon=True).start()
-    app_thread = Thread(target=app.run)
-    app_thread.start()
-    app_thread.join()
+    app.run()
